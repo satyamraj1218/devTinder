cba1d30da2a35bae3702f1f0f99810d07ad62c37
Satyam Raj <satyamraj1218@gmail.com>
2025-06-14 21:01:29 +0530

    feat: enhance error handling in Express app with custom middleware and routes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,35 @@ const express = require("express");
 
 const app = express();
 
-const { adminAuth, userAuth } = require("./middlewares/auth");
+// Error Handling
+// This is one way of error handling. other way is try-catch block
+app.get("/getUserData", (req, res) => {
+  throw new Error("Error Occurred");
+});
+
+app.get("/getUserDetails", (req, res) => {
+  try {
+    throw new Error("Error Occurred");
+  } catch {
+    //log your error
+    res.status(500).send("Something went wrong...Contact support.");
+  }
+});
+
+// sometimes people can miss try catch to handle error, so we can keep something like this to handle errors by default
+// when there are 4 params in handler fn, 1st => err, 2nd => req, 3rd => res, 4th => next
+//use will handle all type of http methods
+
+app.use("/", (err, req, res, next) => {
+  if (err) {
+    //log your error
+    res.status(500).send("Something went wrong. Contact support.");
+  }
+});
+
+//Middlewares
+
+/* const { adminAuth, userAuth } = require("./middlewares/auth");
 
 app.use("/admin", adminAuth);
 
@@ -20,7 +48,7 @@ app.get("/admin/getAllData", (req, res) => {
 
 app.get("/admin/deleteUser", (req, res) => {
   res.send("Deleted a user");
-});
+}); */
 
 app.listen(7777, () => {
   console.log("App is listening on port 7777");
