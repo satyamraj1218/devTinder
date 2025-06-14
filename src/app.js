004d4f0ed42b46dd77fb770c0ac7a88d02b42e10
Satyam Raj <satyamraj1218@gmail.com>
2025-06-14 17:49:36 +0530

    feat: add user routes and responses in Express app

--- src/app.js
+++ src/app.js
@@ -2,9 +2,27 @@ const express = require("express");
 
 const app = express();
 
-// app.use("/", (req, res) => {
-//   res.send("Hello Satyam");
-// });
+app.get("/a{b}cd", (req, res) => {
+  res.send({ firstName: "satyam", lastName: "raj" });
+});
+
+app.get("/user/:userId/:name/:password", (req, res) => {
+  console.log(req.params);
+  // console.log(req.query);
+  res.send({ firstName: "satyam", lastName: "raj" });
+});
+
+app.get("/user", (req, res) => {
+  res.send("Hello from the user route");
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data successfully saved to the database");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data successfully deleted from the database");
+});
 
 app.use("/home", (req, res) => {
   res.send("Hello from the dashboard");
@@ -12,6 +30,10 @@ app.use("/home", (req, res) => {
 
 app.use("/test", (req, res) => {
   res.send("Hello from the test servers");
+});
+
+app.use("/", (req, res) => {
+  res.send("Hello Satyam");
 });
 
 app.listen(7777, () => {
