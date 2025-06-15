38ade82804b93c3099c2ee0a85446ee700844bf8
Satyam Raj <satyamraj1218@gmail.com>
2025-06-15 11:29:33 +0530

    feat: add user and feed APIs to retrieve user details and all users from the database

--- src/app.js
+++ src/app.js
@@ -5,6 +5,30 @@ const app = express();
 
 app.use(express.json());
 
+//API: To get a user from DB based on emailId
+app.get("/user", async (req, res) => {
+  const userEmail = req?.body?.emailId;
+
+  try {
+    const userDetails = await User.findOne({ emailId: userEmail });
+    if (userDetails) res.send(userDetails);
+    else res.status(404).send("User not found");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
+//Feed API : To get all the users from db based
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length != 0) res.send(users);
+    else res.status(404).send("No users found");
+  } catch (err) {
+    res.status(400).send("Something went wrong");
+  }
+});
+
 app.post("/signup", async (req, res) => {
   const user = new User(req?.body);
 
